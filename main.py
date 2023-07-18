6f902f6c8a454e484cb02dbd7137ea005c8dd094
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP-2.lan>
2023-07-18 14:50:20 -0400

    added pages

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from fpdf import FPDF 
 
-pdfp = FPDF(orientation='P', unit='mm', format='A4')
+pdf = FPDF(orientation='P', unit='mm', format='A4')
 
+pdf.add_page()
+
+pdf.set_font(family='Times', size=12)
+pdf.cell(w=0, h=12, txt='Hello there', align="L", ln=1, border=1)
+pdf.cell(w=0, h=12, txt='Hi there', align="L", ln=1, border=1)
+
+pdf.add_page()
+
+pdf.set_font(family='Times', size=12)
+pdf.cell(w=0, h=12, txt='Hello there', align="L", ln=1, border=1)
+pdf.cell(w=0, h=12, txt='Hi there', align="L", ln=1, border=1)
+
+pdf.output('output.pdf')
